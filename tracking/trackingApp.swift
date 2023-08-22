198507743b03ae52f288cc52bdd32e5f12be43b1
Fernando Salom <fer@rudo.es>
2023-08-22 12:56:50 +0200

    Add initial project

---
+++ tracking/trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackingApp.swift
+//  tracking
+//
+//  Created by Fernando Salom Carratala on 22/8/23.
+//
+
+import SwiftUI
+
+@main
+struct trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
